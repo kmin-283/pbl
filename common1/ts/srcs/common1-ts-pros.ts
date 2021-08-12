183a9d289a15f4b0084ed182ebf31718116b53a8
yalpal yappala <rudgks114@gmail.com>
2021-08-12 23:15:22 +0900

    feat: 타입스크립트로 코드 전환

--- common1/ts/srcs/common1-ts-pros.ts
+++ common1/ts/srcs/common1-ts-pros.ts
@@ -1,16 +1,16 @@
-(function () {
-  let a = 10; // 변수 할당 방법
-
-  function sum(a, b) { // 함수 선언 방법
+(function (): void {
+  let a: number = 10; // 변수 할당 방법
+  
+  function sum(a: number, b: number): number { // 함수 선언 방법
     if (a < 0) { // 조건문 작성 방법
       a *= -1;
     }
     return (a+b);
   }
-
-  console.log(sum(5,5)); // 출력 방법 결과값: 10
-
-  for (let i=0; i<10; i++) { // 기본적인 반복문 작성 방법
+  
+  console.log(sum(5,5)); // 출력 방법
+  
+  for (let i=0; i<10; i++) { // 반복문 작성 방법
     console.log(i);
   }
 
