61e5979f8b454e9755645a12f857fdbb342dd474
yalpal yappala <rudgks114@gmail.com>
2021-08-13 11:47:15 +0900

    feat: 불변객체 생성방법 추가

--- common1/js_cpp/bacis_cons.js
+++ common1/js_cpp/bacis_cons.js
@@ -32,4 +32,19 @@
   
   console.log(getName(person)); // kmin
   console.log(getName(noNamePerson))  // undefined
+
+
+  // 진짜 읽기만 가능한 const 배열
+  const doctorList = ['doctor1', 'doctor2', 'doctor3', 'doctor4'];
+  // const를 변경하려는 시도이므로 에러
+  // doctorList = ['patient1', 'patient2', 'patient3', 'patient4'];
+
+  doctorList.push('patient1'); // 가능
+  console.log(doctorList); // ['doctor1', 'doctor2', 'doctor3', 'doctor4', 'patient1'];
+
+  const ReadonlyDoctorList = ['doctor1', 'doctor2', 'doctor3', 'doctor4'];
+  Object.freeze(ReadonlyDoctorList);
+  
+  ReadonlyDoctorList.push('patient1');
+  console.log(ReadonlyDoctorList);
 })();
--- common1/ts/srcs/basic_pros.ts
+++ common1/ts/srcs/basic_pros.ts
@@ -24,11 +24,24 @@
     console.log(value); // kmin 27 korea
   }
 
-  const arr = [1, "hello", false];
+  const arr = [1, 'hello', false];
   /* 반복 가능한 객체
   (Array, Map, Set, String, TypedArray, arguments)
   를 순회하는 반복문 */
   for (const value of arr) {
     console.log(value);
   }
+
+  // 진짜 읽기만 가능한 const 배열
+  const doctorList = ['doctor1', 'doctor2', 'doctor3', 'doctor4'];
+  // const를 변경하려는 시도이므로 에러
+  // doctorList = ['patient1', 'patient2', 'patient3', 'patient4'];
+
+  doctorList.push('patient1'); // 가능
+  console.log(doctorList); // ['doctor1', 'doctor2', 'doctor3', 'doctor4', 'patient1'];
+
+  const ReadonlyDoctorList: Readonly<string[]> = ['doctor1', 'doctor2', 'doctor3', 'doctor4'];
+  
+  // ReadonlyDoctorList.push('patient1');
+  console.log(ReadonlyDoctorList);
 })();
